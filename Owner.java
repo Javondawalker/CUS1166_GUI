7f80c38cc93d3953e41be724b99c9091d4966ebd
Javondawalker <77946669+Javondawalker@users.noreply.github.com>
2026-02-22 23:16:45 -0500

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -3,35 +3,25 @@ public class Owner extends User {
     private String vehicleModel;
     private String vehicleMake;
     private int vehicleYear;
-    private String arrivalTime;
-    private String departureTime;
+    private String residencyTime;
 
-    public Owner(String ownerID, String vehicleID, String vehicleModel, String vehicleMake,
-                 int vehicleYear, String arrivalTime, String departureTime) {
+    public Owner(String ownerID, String vehicleID, String vehicleModel, String vehicleMake, int vehicleYear, String residencyTime) {
         super(ownerID);
         this.vehicleID = vehicleID;
         this.vehicleModel = vehicleModel;
         this.vehicleMake = vehicleMake;
         this.vehicleYear = vehicleYear;
-        this.arrivalTime = arrivalTime;
-        this.departureTime = departureTime;
+        this.residencyTime = residencyTime;
     }
 
-    public String getVehicleID()    { return vehicleID; }
-    public String getVehicleModel() { return vehicleModel; }
-    public String getVehicleMake()  { return vehicleMake; }
-    public int getVehicleYear()     { return vehicleYear; }
-    public String getArrivalTime()  { return arrivalTime; }
-    public String getDepartureTime(){ return departureTime; }
-
+    @Override
     public String fileText() {
-        return "Owner ID: " + ID +
-               " | Timestamp: " + time +
-               " | Vehicle ID: " + vehicleID +
-               " | Vehicle Model: " + vehicleModel +
-               " | Vehicle Make: " + vehicleMake +
-               " | Vehicle Year: " + vehicleYear +
-               " | Arrival Time: " + arrivalTime + " hr/min" +
-               " | Departure Time: " + departureTime + " hr/min";
+        return "Owner ID: " + ID
+                + " | Time: " + time
+                + " | Vehicle ID: " + vehicleID
+                + " | Vehicle model: " + vehicleModel
+                + " | Vehicle make: " + vehicleMake
+                + " | Vehicle year: " + vehicleYear
+                + " | Approx. residency time: " + residencyTime;
     }
 }
